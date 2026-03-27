c876c5a36abd2d4a0547a7f8c60109f7bf21e2f4
Cam Brooks <cambrooks3393@gmail.com>
2026-03-26 20:15:54 -0400

    fix: typed ph actually uses typed ph

--- examples/arduino/i2c/typed/read_ph/read_ph.ino
+++ examples/arduino/i2c/typed/read_ph/read_ph.ino
@@ -1,5 +1,5 @@
 /*
-Purpose: simple Arduino I2C pH read using the C++ wrapper.
+Purpose: simple typed Arduino I2C pH read.
 Defaults: Wire on the board default pins and address 99.
 Assumptions: the device is a pH circuit already in I2C mode.
 Next: read ../../commissioning/inspect_device/inspect_device.ino for setup checks.
@@ -7,14 +7,14 @@ Next: read ../../commissioning/inspect_device/inspect_device.ino for setup check
 
 #include <Wire.h>
 
-#include <ezo_i2c.hpp>
 #include <ezo_i2c_arduino_wire.h>
+#include <ezo_ph.h>
 
 static const unsigned long STARTUP_SETTLE_MS = 1000UL;
 static const uint8_t PH_I2C_ADDRESS = 99U;
 
 static ezo_arduino_wire_context_t wire_context;
-static ezo_i2c::Device ph_device;
+static ezo_i2c_device_t device;
 static unsigned long startup_started_at_ms = 0;
 static uint8_t read_requested = 0;
 
@@ -36,8 +36,9 @@ static void fail_fast(const char *step, ezo_result_t result) {
 #define CHECK_OK(step, expr) fail_fast(step, (expr))
 
 static void request_reading() {
-  ezo_i2c::TimingHint hint;
-  CHECK_OK("send_read", ph_device.send_read(&hint));
+  ezo_timing_hint_t hint;
+
+  CHECK_OK("send_read", ezo_ph_send_read_i2c(&device, &hint));
   delay(hint.wait_ms);
 }
 
@@ -46,15 +47,13 @@ void setup() {
   Wire.begin();
 
   CHECK_OK("init_wire_context", ezo_arduino_wire_context_init(&wire_context, &Wire));
-  CHECK_OK("init_device", ph_device.init(PH_I2C_ADDRESS, ezo_arduino_wire_transport(), &wire_context));
+  CHECK_OK("init_device",
+           ezo_device_init(&device, PH_I2C_ADDRESS, ezo_arduino_wire_transport(), &wire_context));
   startup_started_at_ms = millis();
 }
 
 void loop() {
-  char response[32];
-  size_t response_len = 0;
-  ezo_i2c::DeviceStatus status = EZO_STATUS_UNKNOWN;
-  double value = 0.0;
+  ezo_ph_reading_t reading;
 
   if (read_requested == 0U) {
     if ((unsigned long)(millis() - startup_started_at_ms) < STARTUP_SETTLE_MS) {
@@ -66,18 +65,10 @@ void loop() {
     return;
   }
 
-  CHECK_OK("read_response", ph_device.read_response(response, sizeof(response), &response_len, &status));
+  CHECK_OK("read_response", ezo_ph_read_response_i2c(&device, &reading));
 
-  if (status == EZO_STATUS_SUCCESS &&
-      ph_device.parse_double(response, response_len, &value) == EZO_OK) {
-    Serial.print("ph=");
-    Serial.println(value, 3);
-  } else {
-    Serial.print("device_status_name=");
-    Serial.println(ezo_device_status_name(status));
-    Serial.print("device_status_code=");
-    Serial.println((int)status);
-  }
+  Serial.print("ph=");
+  Serial.println(reading.ph, 3);
 
   delay(1000);
   request_reading();
