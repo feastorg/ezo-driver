fef1a895bc82118e77fbc299b9fda72c6ab5fbfb
Cameron Brooks <cambrooks3393@gmail.com>
2026-03-30 00:21:12 -0400

    chore: strip temp debugging from linux examples

--- examples/linux/common/example_control.c
+++ examples/linux/common/example_control.c
@@ -9,91 +9,23 @@ static void ezo_example_print_shared_control_result(const char *step, ezo_result
   printf("%s_result_code=%d\n", step, (int)result);
 }
 
-static void ezo_example_print_text_debug(const char *label, const uint8_t *buffer, size_t len) {
-  size_t i = 0;
-
-  printf("%s=", label);
-  for (i = 0; i < len; ++i) {
-    const unsigned value = (unsigned)buffer[i];
-
-    if (value == '\\') {
-      printf("\\\\");
-    } else if (value == '\r') {
-      printf("\\r");
-    } else if (value == '\n') {
-      printf("\\n");
-    } else if (value == '\t') {
-      printf("\\t");
-    } else if (value >= 32U && value <= 126U) {
-      printf("%c", (int)value);
-    } else {
-      printf("\\x%02X", value);
-    }
-  }
-  printf("\n");
-}
-
-static void ezo_example_print_hex_debug(const char *label, const uint8_t *buffer, size_t len) {
-  size_t i = 0;
-
-  printf("%s=", label);
-  for (i = 0; i < len; ++i) {
-    if (i > 0) {
-      printf(" ");
-    }
-    printf("%02X", (unsigned)buffer[i]);
-  }
-  printf("\n");
-}
-
 static ezo_result_t ezo_example_query_name_i2c(ezo_i2c_device_t *device,
                                                ezo_product_id_t product_id,
                                                ezo_control_name_t *name_out) {
   ezo_timing_hint_t hint;
-  char text_buffer[EZO_I2C_MAX_TEXT_RESPONSE_CAPACITY];
-  size_t text_len = 0;
-  ezo_device_status_t device_status = EZO_STATUS_UNKNOWN;
-  ezo_result_t send_result = EZO_OK;
-  ezo_result_t read_result = EZO_OK;
-  ezo_result_t parse_result = EZO_OK;
+  ezo_result_t result = EZO_OK;
 
   if (device == NULL || name_out == NULL) {
     return EZO_ERR_INVALID_ARGUMENT;
   }
 
-  send_result = ezo_control_send_name_query_i2c(device, product_id, &hint);
-  if (send_result != EZO_OK) {
-    ezo_example_print_shared_control_result("shared_control_name_query_send", send_result);
-    return send_result;
+  result = ezo_control_send_name_query_i2c(device, product_id, &hint);
+  if (result != EZO_OK) {
+    return result;
   }
 
   ezo_example_wait_hint(&hint);
-  read_result =
-      ezo_read_response(device, text_buffer, sizeof(text_buffer), &text_len, &device_status);
-  if (read_result != EZO_OK) {
-    ezo_example_print_shared_control_result("shared_control_name_query_read", read_result);
-    printf("shared_control_name_query_device_status=%s\n",
-           ezo_device_status_name(device_status));
-    printf("shared_control_name_query_device_status_code=%u\n", (unsigned)device_status);
-    return read_result;
-  }
-
-  parse_result = ezo_control_parse_name(text_buffer, text_len, name_out);
-  if (parse_result != EZO_OK) {
-    printf("shared_control_name_query_device_status=%s\n",
-           ezo_device_status_name(device_status));
-    printf("shared_control_name_query_device_status_code=%u\n", (unsigned)device_status);
-    printf("shared_control_name_query_text_len=%u\n", (unsigned)text_len);
-    ezo_example_print_text_debug("shared_control_name_query_text", (const uint8_t *)text_buffer,
-                                 text_len);
-    ezo_example_print_hex_debug("shared_control_name_query_text_hex",
-                                (const uint8_t *)text_buffer,
-                                text_len);
-    ezo_example_print_shared_control_result("shared_control_name_query_parse", parse_result);
-    return parse_result;
-  }
-
-  return EZO_OK;
+  return ezo_control_read_name_i2c(device, name_out);
 }
 
 ezo_result_t ezo_example_query_info_i2c(ezo_i2c_device_t *device, ezo_device_info_t *info_out) {
